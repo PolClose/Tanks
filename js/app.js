2e8ccf45a23582f0ff3b6522b60ce09ef995c7fe
Paweł <syndromx@o2.pl>
2019-03-02 00:48:16 +0100

    ostylowany czolg

--- js/app.js
+++ js/app.js
@@ -1,18 +1,27 @@
 class Game {
   constructor() {
     this.gameState = false;
-    this.counterTanks = 0;
-    this.counterLife = 10;
     this.time = 0;
     this.gameInterval;
 
     this.tanksPosition = [
-      {"x":50,"y":0},
-      {"x":100,"y":0},
-      {"x":150,"y":0},
+      {"x":50,"y":105,"z":9},
+      {"x":150,"y":100,"z":9},
+      {"x":250,"y":100,"z":9},
+      {"x":350,"y":115,"z":9},
+      {"x":500,"y":165,"z":19},
+      {"x":400,"y":180,"z":19},
+      {"x":300,"y":195,"z":19},
+      {"x":200,"y":215,"z":19},
+      {"x":100,"y":290,"z":29},
+      {"x":200,"y":295,"z":29},
+      {"x":300,"y":300,"z":29},
+      {"x":400,"y":305,"z":29},
     ];
 
     this.tanks = [];
+    this.counterTanks = [];
+    this.counterLife = 10;
   }
 
   startGame() {
@@ -39,7 +48,7 @@ class Game {
         $(this).data("state","stop");
       }
     });
-    let counterTanks = $('<div class="counterTanks">Ilosc Czołgów'+this.counterTanks+'</div>');
+    let counterTanks = $('<div class="counterTanks">Ilosc Czołgów'+this.counterTanks.length+'</div>');
     let counterLife = $('<div class="counterLife">Życie'+this.counterLife+'</div>');
     $(".menu").append(pause).append(counterTanks).append(counterLife);
 
@@ -64,13 +73,13 @@ console.log(this);
 
   createTank() {
     let tanks = this.tanks;
-    
+    let counterTanksNum = this.counterTanks;
     
     let tanksPosition = this.tanksPosition;
       
     //;
     //let randomTank = Math.floor((Math.random() * this.tanksPosition.length) +1 )-1;
-    if(tanks.length<3){
+    if(tanks.length<12){
       do {
 
          var randomTank = Math.floor((Math.random() * this.tanksPosition.length) +1 )-1;
@@ -92,15 +101,17 @@ console.log(this);
       return result;
     }
       let tank = new Tank();
-      tank.create(this.tanksPosition[randomTank].x,this.tanksPosition[randomTank].y,randomTank);
+      tank.create(this.tanksPosition[randomTank].x,this.tanksPosition[randomTank].y,this.tanksPosition[randomTank].z,randomTank);
     
-
+      console.log(counterTanksNum);
+      
       tanks.push(tank);
       console.log(tanks);
     
       tank.element.on("click",function(e){
       this.remove();
       console.log($(e.target).data("id"));
+      counterTanksNum.push('1');
       
           tanks.splice($(e.target).data("id"), 1);
       
@@ -123,12 +134,13 @@ class Tank {
     this.element;
   }
 
-  create(x,y,id) {
+  create(x,y,z,id) {
     this.position.x=x;
     this.position.y=y;
+    this.position.z=z;
     let playgroung = $(".playground");
-    let tank = $('<div class="tank" data-id="'+id+'"></div>');
-    tank.css('top', this.position.y).css('left', this.position.x);
+    let tank = $('<div class="tank" data-id="'+id+'"><circle class="tower" /></div>');
+    tank.css('top', this.position.y).css('left', this.position.x).css('z-index', this.position.z);
     this.element = tank;
     playgroung.append(tank);
     this.id = id;
