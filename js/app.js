63f8ecb7f6242b4d3cb48061f391ecae858f227f
Paweł <syndromx@o2.pl>
2019-03-09 23:56:56 +0100

    cos dalej

--- js/app.js
+++ js/app.js
@@ -1,8 +1,19 @@
 class Game {
   constructor() {
     this.gameState = false;
+    this.s = 0;
+    this.m = 0;
     this.time = 0;
     this.gameInterval;
+    this.interval = 1000;
+    // this.intervalVal = setInterval(()=>{
+    //   if (game.time < 10){
+    //     this.interval = 2000
+    //   }else  {
+    //     this.interval = 1000
+    //     console.log("cuda cuda");
+    //   }
+    // },1000);
     this.tanksPosition = [
       {"x":50,"y":105,"z":9},
       {"x":150,"y":100,"z":9},
@@ -19,7 +30,7 @@ class Game {
     ];
 
     this.tanks = [];
-    this.counterTanks = 0;
+    this.counterTanks = [];
     this.counterLife = 10;
   }
 
@@ -32,46 +43,85 @@ class Game {
     let hill2 = $('<div class="hill2"><div>');
     let hill3 = $('<div class="hill3"><div>');
     
-    $(".playground").append(hill1).append(hill2).append(hill3);
+    $(".playground").removeClass("hide").append(hill1).append(hill2).append(hill3);
     
-    let pause = $('<button class="pause" data-state="stop">Stop</div>');
+    // let pause = $('<button class="pause" data-state="stop">Stop</div>');
 
-    pause.on("click", function(){
-      if($(this).data("state")=="stop"){
-        gameState = false;
-        $(this).text("resume");
-        $(this).data("state","resume");
-      }else{
-        gameState = true;
-        $(this).text("stop");
-        $(this).data("state","stop");
-      }
-    });
+    // pause.on("click", function(){
+    //   if($(this).data("state")=="stop"){
+    //     gameState = false;
+    //     $(this).text("resume");
+    //     $(this).data("state","resume");
+    //   }else{
+    //     gameState = true;
+    //     $(this).text("stop");
+    //     $(this).data("state","stop");
+    //   }
+    // });
     
-    let timerdiv = $('<div class="timerdiv">czas:'+this.time+'</div>');
-    let counterTanks = $('<div class="counterTanks">Ilosc Czołgów'+this.counterTanks+'</div>');
-    let counterLife = $('<div class="counterLife">Życie'+this.counterLife+'</div>');
-    $(".menu").append(pause).append(counterTanks).append(counterLife).append(timerdiv);
+    let timerDiv = $('<div class="timerDiv">czas:'+this.m+':0'+this.s+'</div>');
+    let counterTanksDiv = $('<div class="counterTanks">Ilość Czołgów:'+this.counterTanks.length+'</div>');
+    let counterLifeDiv = $('<div class="counterLife">Życie:'+this.counterLife+'</div>');
+    // let levelDiv = $('<div class="level">LEVEL 1</div>');
+    $(".menu").append(counterTanksDiv).append(counterLifeDiv).append(timerDiv);
+    // $(".playground").append(levelDiv);
 
-    // this.createTank();
+    // this.levelInterval = setInterval(()=>{
+    //   if( this.time >=10 && this.time < 60){
+    //     $(".level").html("LEVEL 2");
+    //   }
+    // },10000);
 
-    this.gameInterval = setInterval(()=>{
-      let time = this.time;
+    this.timeInterval = setInterval(()=>{
       if (gameState){
-        this.createTank()
+        this.timer();
         
       }
     },1000);
-
-  }
+    
+    this.gameInterval = setInterval(()=>{
+      if (gameState){
+        this.createTank();
+      }
+    }, this.interval);
   
+    
+
+    this.medInterval = setInterval(()=>{
+      this.createMed();
+    this.medTimeout = setTimeout(()=>{$(".med").remove()},5000)
+    },15000)  
+    
+    //this.endGame();
+    
+  }
+  timer() {
+    this.time++;
+    this.s++;
+    
+    if (this.s==60){
+      this.s = 0;
+      this.m++;
+    }
+    if (this.s < 10 ) { 
+      $(".timerDiv").html('czas:'+this.m+':0'+this.s); }else{
+      $(".timerDiv").html('czas:'+this.m+':'+this.s); 
+    }
+    // if (this.time < 10){
+    //   this.intervalVal = 4000
+    // }else  {
+    //   this.intervalVal = 1000
+    // }
+    
+  }
+
   createTank() {
     let tanks = this.tanks;
+    let counterTanks = this.counterTanks;
     
-    let tanksPosition = this.tanksPosition;
       
     //let randomTank = Math.floor((Math.random() * this.tanksPosition.length) +1 )-1;
-    if(tanks.length<12){
+    if(tanks.length < game.tanksPosition.length){
       do {
 
          var randomTank = Math.floor((Math.random() * this.tanksPosition.length) +1 )-1;
@@ -82,10 +132,10 @@ class Game {
     function checkTanks(id){
       let result = false;
       tanks.forEach(function(e,i){
-        console.log('id',e.id);
+        // console.log('id',e.id);
         if(e.id == id){
           result = true;
-          console.log('jest');
+          // console.log('jest');
           //debugger;
         }
         
@@ -93,44 +143,103 @@ class Game {
       return result;
     }
       let tank = new Tank();
+      
       tank.create(this.tanksPosition[randomTank].x,this.tanksPosition[randomTank].y,this.tanksPosition[randomTank].z,randomTank);
-    
+      let helpThis = this;
       tanks.push(tank);
+     
       
       tank.element.on("click",function(e){
-      this.remove();
-      console.log($(e.target).data("id"));
-      tanks.splice($(e.target).data("id"), 1);
+        clearInterval(tank.interval);
+        this.remove();
+      // console.log($(e.target).data("id"));
+        tanks.splice(helpThis, 1);
+      // console.log(tanks);
+      
+        helpThis.counterTanks.push('0');
+        $('.counterTanks').html('Ilość Czołgów:'+ helpThis.counterTanks.length);
       });
     }
     
   }
+  createMed() {
+      let helpThis = this;
+      let randomMed = Math.floor((Math.random() * this.tanksPosition.length) +1 )-1;
+      let med = new Med();     
+      med.create(this.tanksPosition[randomMed].x,this.tanksPosition[randomMed].y,this.tanksPosition[randomMed].z,randomMed);    
+      
+      med.element.on("click",function(){
+        helpThis.counterLife += 2;
+        $('.counterLife').html("Życie:"+game.counterLife);
+        this.remove();
+      });
+      
+      
+    }
+  endGame() {
+    if ( game.counterLife < 1){
+      let gameOver = $('<div class="over">You Loose</div>');
+      let startAgain = $('<button class="startGame"><a href="index.html">One More Time</a></button>')
+      $(".playground").remove();
+      $(".counterLife").remove();
+      // $(".pause").remove();
+      $(".box").append(gameOver).append(startAgain);
+      
+      
+      game.tanks.forEach((element)=>{
+        clearInterval(element.interval);
+      });
+      game.tanks = [];
+      clearInterval(game.gameInterval);
+      clearInterval(game.timeInterval);
+    }  
+
+  }
+  
+
 }
 
 class Tank {
   constructor(){
-    this.life = 1;
-    this.position = {
-      "x": 0,
-      "y": 0
-    };
+    this.position = {};
     this.id;
     this.element;
+    this.interval = setInterval(function(){
+      game.counterLife--;
+      $('.counterLife').html("Życie:"+game.counterLife);
+       game.endGame();
+       
+       },4000);
+       
   }
 
   create(x,y,z,id) {
     this.position.x=x;
     this.position.y=y;
     this.position.z=z;
-    let playgroung = $(".playground");
     let tank = $('<div class="tank" data-id="'+id+'"><circle class="tower" /></div>');
     tank.css('top', this.position.y).css('left', this.position.x).css('z-index', this.position.z);
     this.element = tank;
-    playgroung.append(tank);
+    $(".playground").append(tank);
     this.id = id;
-
   }
-
+}
+class Med {
+  constructor(){
+    this.position = {};
+    this.id;
+    this.element;
+  }
+  create(x,y,z,id) {
+    this.position.x=x;
+    this.position.y=y;
+    this.position.z=z;
+    let med = $('<div class="med"></div>');
+    med.css('top', this.position.y).css('left', this.position.x).css('z-index', this.position.z);
+    this.element = med;
+    $(".playground").append(med);
+    this.id = id;
+  }
 }
 
 let game = new Game();
